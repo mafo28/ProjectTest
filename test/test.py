47851088e417b7c41ae6810d6164a17e247bc3fb
Merveille <mafo@rhrk.uni-kl.de>
2024-05-18 23:21:23 +0200

    update

--- test/test.py
+++ test/test.py
@@ -1,17 +1,52 @@
 import unittest
-import pickle
-import pandas as pd
+import json
+from flask import Flask
+from flask.testing import FlaskClient
 
-class TestModel(unittest.TestCase):
+# Importer l'application Flask
+from main import app, InputData
+
+class TestAPI(unittest.TestCase):
     def setUp(self):
-        with open('../random_forest_model.pkl', 'rb') as file:
-            self.model = pickle.load(file)
+        # Configurer l'application pour le test
+        app.config['TESTING'] = True
+        self.client = app.test_client()
     
-    def test_prediction(self):
-        data = {'feature1': [value1], 'feature2': [value2], ...}  # Remplacez par vos features
-        df = pd.DataFrame(data)
-        prediction = self.model.predict(df)
-        self.assertIsNotNone(prediction)
+    def test_accueil(self):
+        # Test de la route d'accueil
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertEqual(data['message'], 'Bienvenue sur l\'API de prediction du remboursement de credit')
+
+    def test_predire_valid(self):
+        # Test de la route de prédiction avec des données valides
+        valid_data = {
+            "installment": 250.0,
+            "revol_util": 45.0,
+            "revol_bal": 5000.0,
+            "fico": 700.0,
+            "purpose_debt_consolidation": 1.0
+        }
+        response = self.client.post('/', json=valid_data)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        self.assertIn('prediction', data)
+
+    def test_predire_invalid(self):
+        # Test de la route de prédiction avec des données invalides
+        invalid_data = {
+            "installment": "invalid_data",  # Installment devrait être un float
+            "revol_util": 45.0,
+            "revol_bal": 5000.0,
+            "fico": 700.0,
+            "purpose_debt_consolidation": 1.0
+        }
+        response = self.client.post('/', json=invalid_data)
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data)
+        self.assertIn('error', data)
 
 if __name__ == '__main__':
     unittest.main()
+
